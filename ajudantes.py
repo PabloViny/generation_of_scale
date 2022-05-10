9a0ac749614bab26514140a407d9d8f405f068c9
Ana <pablovinissantos@outlook.com>
2022-05-10 12:13:18 -0300

    add lista_ajudantes

---
+++ ajudantes.py
@@ -0,0 +1,13 @@
+kell_ped = {'Nome':'Raquel','Turma':'Pedaço'}
+gisele = {'Nome':'Gisele','Turma':'Pedaço'}
+brisa = {'Nome':'Brisa','Turma':'Pedaço'}
+lane = {'Nome':'Cailane','Turma':'Pedaço'}
+nara_ped = {'Nome':'Nara','Turma':'Pedaço'}
+bella_ped = {'Nome':'Bella','Turma':'Pedaço'}
+#--------------------------------------------------------
+kailan = {'Nome':'Kailan','Turma':'Ovelha'}
+bella_ove = {'Nome':'Bella','Turma':'Ovelha'}
+kell_ove = {'Nome':'Raquel','Turma':'Ovelha'}
+pablo = {'Nome':'Pablo','Turma':'Ovelha'}
+nara_ove = {'Nome':'Nara','Turma':'Ovelha'}
+
