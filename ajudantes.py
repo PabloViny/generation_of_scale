43c4cae040d7acccaa557c8c59c41fbe40947227
Ana <pablovinissantos@outlook.com>
2022-07-03 23:33:00 -0300

    updating list

--- ajudantes.py
+++ ajudantes.py
@@ -1,13 +1,10 @@
-kell_ped = {'Nome':'Raquel','Turma':'Pedaço'}
 gisele = {'Nome':'Gisele','Turma':'Pedaço'}
 brisa = {'Nome':'Brisa','Turma':'Pedaço'}
 lane = {'Nome':'Cailane','Turma':'Pedaço'}
-nara_ped = {'Nome':'Nara','Turma':'Pedaço'}
-bella_ped = {'Nome':'Bella','Turma':'Pedaço'}
 #--------------------------------------------------------
 kailan = {'Nome':'Kailan','Turma':'Ovelha'}
-bella_ove = {'Nome':'Bella','Turma':'Ovelha'}
-kell_ove = {'Nome':'Raquel','Turma':'Ovelha'}
-pablo = {'Nome':'Pablo','Turma':'Ovelha'}
-nara_ove = {'Nome':'Nara','Turma':'Ovelha'}
+#--------------------------------------------------------
+kell = {'Nome':'Raquel','Turma': ['Pedaço','Ovelha'],'Status': 0}
+nara = {'Nome':'Nara','Turma': ['Pedaço','Ovelha'],'Status': 0}
+bella = {'Nome':'Bella','Turma': ['Pedaço', 'Ovelha'],'Status': 0}
 
